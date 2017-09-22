8b3b573b6560c6d23cd5fa63e6e36444b78851a6
moving7 <1115123081@qq.com>
2017-09-22 22:23:20 +0800

    hello

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 项目常量文件
+ * User: find35.com
+ * Date: 15/12/30
+ * Time: 下午8:49
+ */
+
+define('HOST','127.0.0.1');//服务器地址
+define('UNAME','user1');//数据库账号
+define('UPASS','123456');//数据库密码
+define('DBNAME','test1');//操作的数据库名
